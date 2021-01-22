5634cfef8cacb9803a762dc5914e7e28fc8f7c9e
Paul Hart <jphart@glentech.com>
2021-01-21 16:28:10 -0800

    Initial Commit

---
+++ Shared/BC_COVID_19_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BC_COVID_19_IOSApp.swift
+//  Shared
+//
+//  Created by Paul Hart on 2021-01-21.
+//
+
+import SwiftUI
+
+@main
+struct BC_COVID_19_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
